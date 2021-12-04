b584ac2b157c916fe2642df8682aa3dced329a9c
Pratik Narkar <pratik_narkar@persistent.com>
2021-12-04 19:39:38 +0530

    - Added seed data, gem fast_jsonapi, ruby-version file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+airlines = Airline.create([
+{
+	name: 'United Airline 1',
+	image_url: '../app/assets/images/airlines/1.jpg'
+},
+{
+	name: 'United Airline 2',
+	image_url: '../app/assets/images/airlines/2.jpg'
+},
+{
+	name: 'United Airline 3',
+	image_url: '../app/assets/images/airlines/3.jpg'
+},
+{
+	name: 'United Airline 4',
+	image_url: '../app/assets/images/airlines/1.jpg'
+},
+{
+	name: 'United Airline 5',
+	image_url: '../app/assets/images/airlines/2.jpg'
+},
+{
+	name: 'United Airline 6',
+	image_url: '../app/assets/images/airlines/3.jpg'
+}
+])
+
+Review.create([
+  {
+	title: 'Great Airline',
+	description: 'I had a lovely time',
+	score: 5,
+	airline: airlines.first	
+  },
+  {
+	title: 'Bad Airline',
+	description: 'I had a bad time',
+	score: 1,
+	airline: airlines.first	
+  },
+
+]
+
+)
